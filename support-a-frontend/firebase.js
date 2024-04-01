8b805b93d03ab49f80b1ab6e331b47d11c24701e
MichaelMondelice <michael1.mondelice@famu.edu>
2024-04-01 17:55:20 -0400

    LoginScreen Fix

---
+++ support-a-frontend/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBlvwFDHHGCKRAOZSygdbRzRZKPZQnBrrw",
+    authDomain: "support-a-rattler.firebaseapp.com",
+    projectId: "support-a-rattler",
+    storageBucket: "support-a-rattler.appspot.com",
+    messagingSenderId: "191296613393",
+    appId: "1:191296613393:web:edc2f731bcbe71d67e95b0",
+    measurementId: "G-KFM4Y8NEWR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
